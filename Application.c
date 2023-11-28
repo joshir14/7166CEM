ef6ff178c2d5d78678830f16c18ba88a5d4a3e79
joshir14 <joshir14@coventry.ac.uk>
2023-11-29 00:59:57 +0530

    Modified frame process functions

--- Application.c
+++ Application.c
@@ -1,66 +1,68 @@
 #include "Application.h"
 
-void copyData(struct can_frame* frame, uint8_t *currentDataBuffer, int dlc);
-int dlc;
-
-
 int main()
 {
 	const int socket_id = can_connect("vcan0",0);
 	float cf1, cr1;
-	struct candata_vcu_battery_t VBATT;
+	uint8_t count1, count2, count3;
 	struct can_frame frame, TxFrame;
 	float batteryVoltage;
-	struct candata_vcu_wheel_speeds_t wheelSpeed;
-	struct candata_ai_drive_request_t driveRequest;
+	int dlc;
 	uint16_t rr_wheelSpeed, rl_wheelSpeed, fr_wheelSpeed, fl_wheelSpeed;
 	float frontCurrent, rearCurrent;
 	float steeringReq, rearTrq, frontTrq;
-	struct candata_motor_current_t currentData;
+
 	uint8_t currentDataBuffer[8];
 	int tf1, tr1;
 	while(1)
 	{
-		if(can_read(socket_id,&frame)==1)
+		if(can_read(socket_id,&frame) == 1)
 		{
 			if((frame.can_id == 0x526) && (frame.can_dlc > 0))
 			{
-				vcu_battery_frame_process(&VBATT, frame, &batteryVoltage);
+				count1 = count1 + 1;
+				vcu_battery_frame_process(frame, &batteryVoltage);
 				printf("Battery voltage is %f\n", batteryVoltage);
 			}
 			else if((frame.can_id == 0x521) && (frame.can_dlc > 0))
 			{
-				ai_drive_request_frame_process(&driveRequest, frame, &frontTrq, &rearTrq, &steeringReq);
+				count2 = count2 + 1;
+				ai_drive_request_frame_process(frame, &frontTrq, &rearTrq, &steeringReq);
 				printf("Front torque is %f\n", frontTrq);
 				printf("Rear torque is %f\n", rearTrq);
 				printf("Steering request is %f\n", steeringReq);
 			}
 			else if((frame.can_id == 0x525) && (frame.can_dlc > 0))
 			{
-				vcu_wheel_speed_frame_process(&wheelSpeed, frame, &rr_wheelSpeed, &rl_wheelSpeed, &fr_wheelSpeed, &fl_wheelSpeed);
+				count3 = count3 + 1;
+				vcu_wheel_speed_frame_process(frame, &rr_wheelSpeed, &rl_wheelSpeed, &fr_wheelSpeed, &fl_wheelSpeed);
 				printf("Front right wheel speed is %d\n", fr_wheelSpeed);
 				printf("Rear right wheel speed is %d\n", rr_wheelSpeed);
 				printf("Rear left wheel speed is %d\n", rl_wheelSpeed);
 				printf("Front left wheel speed is %d\n", fl_wheelSpeed);
 			}
-			else
+
+			if((count1 > 0) && (count2 > 0) && (count3 > 0))
 			{
+				count1 = 0;
+				count2 = 0;
+				count3 = 0;
+				tf1 = torque_limit_front(fr_wheelSpeed, fl_wheelSpeed, frontTrq, batteryVoltage);
+				tr1 = torque_limit_rear(rr_wheelSpeed, rl_wheelSpeed, rearTrq, batteryVoltage);
+				printf("Front torque limit %d\n", tf1);
+				printf("Rear torque limit %d\n", tr1);
+				cf1 = calculate_front_current(tf1, batteryVoltage, 300);
+				cr1 = calculate_rear_current(tr1, batteryVoltage, 400);
+				printf("Rear current %f\n", cr1);
+				printf("Front current %f\n", cf1);
+				dlc = process_motor_current_request(cf1, cr1, currentDataBuffer);
+				printf("DLC is %d\n", dlc);
 				
-			}
-			tf1 = torque_limit_front(fr_wheelSpeed, fl_wheelSpeed, frontTrq, batteryVoltage);
-			tr1 = torque_limit_rear(rr_wheelSpeed, rl_wheelSpeed, rearTrq, batteryVoltage);
-			cf1 = calculate_front_current(tf1, batteryVoltage, 300);
-			cr1 = calculate_rear_current(tr1, batteryVoltage, 400);
-			printf("Rear current %f\n", cr1);
-			printf("Front current %f\n", cf1);
-			dlc = process_motor_current_request(4.5, &currentData, 6.77,  currentDataBuffer);
-			printf("DLC is %d\n", dlc);
-			//currentData->front_current = candata_motor_current_front_current_encode(7.8);
-			
-			generate_can_frame(&TxFrame, currentDataBuffer, 0x320, dlc);
-			if(!can_write(socket_id, &TxFrame))
-			{
-				printf("Error\n");
+				generate_can_frame(&TxFrame, currentDataBuffer, 0x320, dlc);
+				if(!can_write(socket_id, &TxFrame))
+				{
+					printf("Error\n");
+				}
 			}
 		}
 	}
--- Application.h
+++ Application.h
@@ -1,10 +1,10 @@
 #include "can_wrap.h"
 #include "candata.h"
 
-void vcu_battery_frame_process(struct candata_vcu_battery_t* VBATT, struct can_frame frame, float* batteryVoltage);
-void ai_drive_request_frame_process(struct candata_ai_drive_request_t* driveRequest, struct can_frame frame, float* frontTrq, float* rearTrq, float* steeringReq);
-void vcu_wheel_speed_frame_process(struct candata_vcu_wheel_speeds_t* wheelSpeed, struct can_frame frame, uint16_t* rr_wheelSpeed, uint16_t* rl_wheelSpeed, uint16_t* fr_wheelSpeed, uint16_t* fl_wheelSpeed);
-int process_motor_current_request(float frontCurrent, struct candata_motor_current_t* currentData, float rearCurrent, uint8_t* currentDataBuffer);
+void vcu_battery_frame_process(struct can_frame frame, float* batteryVoltage);
+void ai_drive_request_frame_process(struct can_frame frame, float* frontTrq, float* rearTrq, float* steeringReq);
+void vcu_wheel_speed_frame_process(struct can_frame frame, uint16_t* rr_wheelSpeed, uint16_t* rl_wheelSpeed, uint16_t* fr_wheelSpeed, uint16_t* fl_wheelSpeed);
+int8_t process_motor_current_request(float frontCurrent, float rearCurrent, uint8_t* currentDataBuffer);
 int torque_limit_rear(uint16_t rr_wheelSpeed, uint16_t rl_wheelSpeed, float rearTrq, float batteryVoltage);
 int torque_limit_front(uint16_t fr_wheelSpeed, uint16_t fl_wheelSpeed, float frontTrq, float batteryVoltage);
 float calculate_front_current(int tr, float v, uint16_t r);
@@ -117,42 +117,49 @@ int torque_limit_rear(uint16_t rr_wheelSpeed, uint16_t rl_wheelSpeed, float rear
 	return tr1;
 }
 
-int process_motor_current_request(float frontCurrent, struct candata_motor_current_t* currentData, float rearCurrent, uint8_t* currentDataBuffer)
+int8_t process_motor_current_request(float frontCurrent, float rearCurrent, uint8_t* currentDataBuffer)
 {
 	int dlc;
-	currentData->front_current = candata_motor_current_front_current_encode(frontCurrent);
-	currentData->rear_current = candata_motor_current_rear_current_encode(rearCurrent);
-	dlc = candata_motor_current_pack(currentDataBuffer, currentData, 4);
+	struct candata_motor_current_t currentData;
+	currentData.front_current = candata_motor_current_front_current_encode(frontCurrent);
+	currentData.rear_current = candata_motor_current_rear_current_encode(rearCurrent);
+	dlc = candata_motor_current_pack(currentDataBuffer, &currentData, 4);
 	return dlc;
 }
 
-void ai_drive_request_frame_process(struct candata_ai_drive_request_t* driveRequest, struct can_frame frame, float* frontTrq, float* rearTrq, float* steeringReq)
+void ai_drive_request_frame_process(struct can_frame frame, float* frontTrq, float* rearTrq, float* steeringReq)
 {
-	if(candata_ai_drive_request_unpack(driveRequest, frame.data, frame.can_dlc)==0)
+	struct candata_ai_drive_request_t driveRequest;
+	if(candata_ai_drive_request_unpack(&driveRequest, frame.data, frame.can_dlc)==0)
 	{
-		*frontTrq = candata_ai_drive_request_front_trq_request_decode(driveRequest->front_trq_request);
-		*rearTrq = candata_ai_drive_request_rear_trq_request_decode(driveRequest->rear_trq_request);
-		*steeringReq = candata_ai_drive_request_steering_request_decode(driveRequest->steering_request);
+		*frontTrq = candata_ai_drive_request_front_trq_request_decode(driveRequest.front_trq_request);
+		*rearTrq = candata_ai_drive_request_rear_trq_request_decode(driveRequest.rear_trq_request);
+		*steeringReq = candata_ai_drive_request_steering_request_decode(driveRequest.steering_request);
 	}
 }
 
-void vcu_battery_frame_process(struct candata_vcu_battery_t *VBATT, struct can_frame frame, float* batteryVoltage)
+void vcu_battery_frame_process(struct can_frame frame, float* batteryVoltage)
 {
-	
-	if(candata_vcu_battery_unpack(VBATT, frame.data, frame.can_dlc) == 0)
+	struct candata_vcu_battery_t VBATT;
+	if(candata_vcu_battery_unpack(&VBATT, frame.data, frame.can_dlc) == 0)
 	{
-		*batteryVoltage = candata_vcu_battery_voltage_decode(VBATT->voltage);
+		*batteryVoltage = candata_vcu_battery_voltage_decode(VBATT.voltage);
+	}
+	else 
+	{
+		*batteryVoltage = 0.0f;
 	}
 }
 
-void vcu_wheel_speed_frame_process(struct candata_vcu_wheel_speeds_t* wheelSpeed, struct can_frame frame, uint16_t* rr_wheelSpeed, uint16_t* rl_wheelSpeed, uint16_t* fr_wheelSpeed, uint16_t* fl_wheelSpeed)
+void vcu_wheel_speed_frame_process(struct can_frame frame, uint16_t* rr_wheelSpeed, uint16_t* rl_wheelSpeed, uint16_t* fr_wheelSpeed, uint16_t* fl_wheelSpeed)
 {
-	if(candata_vcu_wheel_speeds_unpack(wheelSpeed, frame.data, frame.can_dlc)== 0)
+	struct candata_vcu_wheel_speeds_t wheelSpeed;
+	if(candata_vcu_wheel_speeds_unpack(&wheelSpeed, frame.data, frame.can_dlc)== 0)
 	{
-		*rr_wheelSpeed = candata_vcu_wheel_speeds_rr_wheel_speed_decode(wheelSpeed->rr_wheel_speed);
-		*rl_wheelSpeed = candata_vcu_wheel_speeds_rl_wheel_speed_decode(wheelSpeed->rl_wheel_speed);
-		*fr_wheelSpeed = candata_vcu_wheel_speeds_fr_wheel_speed_decode(wheelSpeed->fr_wheel_speed);
-		*fl_wheelSpeed = candata_vcu_wheel_speeds_fl_wheel_speed_decode(wheelSpeed->fl_wheel_speed);
+		*rr_wheelSpeed = candata_vcu_wheel_speeds_rr_wheel_speed_decode(wheelSpeed.rr_wheel_speed);
+		*rl_wheelSpeed = candata_vcu_wheel_speeds_rl_wheel_speed_decode(wheelSpeed.rl_wheel_speed);
+		*fr_wheelSpeed = candata_vcu_wheel_speeds_fr_wheel_speed_decode(wheelSpeed.fr_wheel_speed);
+		*fl_wheelSpeed = candata_vcu_wheel_speeds_fl_wheel_speed_decode(wheelSpeed.fl_wheel_speed);
 	}
 }
 
